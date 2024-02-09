3db6b12e3bd8965781098a949462b19ee4854226
maylique <maylique14@gmail.com>
2024-02-09 09:31:07 +0100

    js done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './assets/pages/home/home'
+import Home from './assets/pages/home/Home'
 import Products from './assets/pages/products/Products'
 import Product from './assets/pages/product/product'
 import Shop from './assets/pages/shop/Shop'
+import Splashscreen from './components/Splashscreen'
+import Onboarding from './components/Onboarding'
 
 function App() {
 
   return (
     <>
     <Routes>
-    <Route path="/" element={<Home />} />
+      <Route path='/' element={<Splashscreen />} />
+      <Route path='/onboarding' element={<Onboarding />} />
+    <Route path="/home" element={<Home />} />
     <Route path="/shop" element={<Shop />} />
     <Route path="/products" element={<Products />} />
     <Route path="/product/:id" element={<Product />} /> 
