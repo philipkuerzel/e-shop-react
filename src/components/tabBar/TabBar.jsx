5c0a62d16f6faf54196bfff1b04af8be555bc382
abothke <andrebothke@gmail.com>
2024-02-11 13:23:19 +0100

    searchmodal done

--- src/components/tabBar/TabBar.jsx
+++ src/components/tabBar/TabBar.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
-import SearchFunction from '../searchFunction/SearchFunction'
+import { useState } from 'react';
 import "./tabBar.css"
 
 const TabBar = () => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const showModal = () => {
+    setModalVisible(true);
+  };
+
+  const hideModal = () => {
+    setModalVisible(false);
+  };
+
+  console.log(modalVisible);
+
   return (
     <>
     <div className='tabBar'>
+      {modalVisible ? ( null ) : (
+        <>
         <button className='homeBtn'><Link to='/home'></Link>home</button>
-        <button className='searchBtn'>{<SearchFunction/>}</button>
-    </div>
+        <button className='searchBtn' onClick={showModal}>search</button>
+        </>
+        )}
+            {modalVisible ? (
+            <div id='searchModal' className='searchModal'>
+              <input type='text' placeholder='search...' />
+              <button id='close' onClick={hideModal}>X</button>
+            </div>
+          ) : null}
+        </div>
     </>
   )
 }
